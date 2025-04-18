4ec711ea5fb92532cc10a4981b61da8acba779aa
JonahThiele <53587938+JonahThiele@users.noreply.github.com>
2025-04-18 02:38:37 +0000

    questions completely accessable, and linked together via endpoints

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,9 @@
 #include <fstream>
 #include <iostream>
 
+//some annoying gets from the query string request 
+#include <bits/stdc++.h>
+
 
 crow::response render_question(inja::json &vars, inja::Environment &env, std::string path) {
     try {
@@ -63,6 +66,51 @@ int main()
     CROW_ROUTE(app, "/q9")([&]{
         return render_question(vars, env, "../data/q9.json");
     });
+
+    CROW_ROUTE(app, "/store").methods(crow::HTTPMethod::POST)([](const crow::request &req){
+        auto body = req.get_body_params();
+        //std::cout << body.get("question_num");
+        long int q = std::strtol(body.get("question_num"),NULL, 10);
+
+        //this works but causes the screen to flash a bit when the redirect doesn't go immediately, Idk how to fix that
+        crow::response  res = crow::response(302, "redirect");
+        switch(q)
+        {
+            case 1:
+                res.set_header("Location", "http://localhost:18080/q2");
+                break;
+            case 2:
+                res.set_header("Location", "http://localhost:18080/q3");
+                break;
+            case 3:
+                res.set_header("Location", "http://localhost:18080/q4");
+                break;
+            case 4:
+                res.set_header("Location", "http://localhost:18080/q5");
+                break;
+            case 5:
+                res.set_header("Location", "http://localhost:18080/q6");    
+                break;
+            case 6:
+                res.set_header("Location", "http://localhost:18080/q7");    
+                break;
+            case 7:
+                res.set_header("Location", "http://localhost:18080/q8");
+                break;
+            case 8:
+                res.set_header("Location", "http://localhost:18080/q9");
+                break;
+            case 9:
+                res.set_header("Location", "http://localhost:18080/q10");
+                break;
+            case 10:
+                res.set_header("Location", "http://localhost:18080/results");
+                break;
+
+        }
+            return res;
+    });
+    
        
     app.port(18080).run();
 }
